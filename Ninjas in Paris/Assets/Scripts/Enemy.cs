cc825b3f0f1f32574af1eb33d37d9ccb656219aa
Yeetus562 <jack.hadskey@gmail.com>
2025-02-02 20:57:41 -0600

    enemies no longer have infinite range

--- Ninjas in Paris/Assets/Scripts/Enemy.cs
+++ Ninjas in Paris/Assets/Scripts/Enemy.cs
@@ -63,7 +63,7 @@ public class Enemy : MonoBehaviour {
                 attackMarker.transform.localScale = new Vector3(1, 1, 1) * ((attackTimer / 3f) + .1f);
                 attackTimer -= Time.deltaTime;
 
-                if (attackTimer <= 0) {
+                if (attackTimer <= 0 && Vector3.Distance(transform.position, player.transform.position) < attackDistance) {
                     player.GetComponent<Player>().takeDamage();
                     state = states.idle;
                 }
